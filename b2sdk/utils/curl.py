34c5a6bcd6b069d3c39204b2ba8d40d39803772d
Krzysztof Kalinowski <krzysztof.kalinowski@reef.pl>
2022-11-25 12:16:14 +0100

    Using email.parser to parse http headers, allowing for streamed HEAD requests

--- b2sdk/utils/curl.py
+++ b2sdk/utils/curl.py
@@ -23,10 +23,11 @@ The idea is:
 """
 
 import collections
+import email.parser
 import threading
 import time
 from dataclasses import dataclass, field
-from functools import partial, partialmethod
+from functools import partialmethod
 from io import BytesIO
 from typing import Dict, Iterator, List, Literal, Optional, Union
 
@@ -115,24 +116,52 @@ class StreamedBytes:
             return result
 
 
+class HeaderReader:
+    # RFC 2086 Section 4.2 states:
+    # HTTP header fields (...) follow the same generic format as that given in Section 3.1 of RFC 822
+    #
+    # email.parser is made to parse RFC 822 headers. Only difference is in encodings. By default,
+    # email.parser uses ASCII encoding while RFC 2086 requires ISO-8859-1. We'll conform to the latter.
+    PARSER = email.parser.HeaderParser()
+    ENCODING = 'iso-8859-1'
+
+    def __init__(self):
+        self.lines = []
+        self.headers: Optional[CaseInsensitiveDict] = None
+
+    def add_header_line(self, line: bytes) -> None:
+        # First line of the header contains http version and status code.
+        # Last line of the header contains just `\r\n`. Removing both.
+        if line.startswith(b'HTTP/') or len(line.strip()) == 0:
+            return
+        self.lines.append(line)
+
+    def parse_headers(self) -> None:
+        if self.headers is not None:
+            return
+
+        parsed_header = self.PARSER.parsestr(
+            b''.join(self.lines).decode(self.ENCODING),
+            headersonly=True,
+        )
+        self.headers = CaseInsensitiveDict(parsed_header.items())
+
+
 @dataclass
 class CurlStreamer:
     curl: pycurl.Curl
     manager: 'CurlManager'
     output: StreamedBytes
-    _headers: CaseInsensitiveDict
+    _headers: HeaderReader
 
     _status_code: Optional[int] = None
     done: threading.Event = field(default_factory=threading.Event)
     timeout_seconds: float = 5.0
 
     def _wait_for_headers(self) -> None:
-        # If we're already done there's nothing to wait for.
-        if self.done.is_set():
-            return
-
         start_time = time.time()
-        while not self.output.first_write_event.is_set():
+        # Stop if we're either done or had any content written.
+        while not self.done.is_set() and not self.output.first_write_event.is_set():
             # `run_iteration` either waits to acquire lock or
             # waits on select, so this can be counted as a form of sleep.
             self.manager.run_iteration()
@@ -153,7 +182,8 @@ class CurlStreamer:
     @property
     def headers(self) -> CaseInsensitiveDict:
         self._wait_for_headers()
-        return self._headers
+        self._headers.parse_headers()
+        return self._headers.headers
 
     def read(self, count: int) -> bytes:
         self.manager.run_iteration()
@@ -166,6 +196,7 @@ class CurlStreamer:
     def close(self) -> None:
         # We won't be able to reclaim the status code once we release the object.
         self._get_and_cache_status_code()
+        self._headers.parse_headers()
         self.curl.close()
         self.done.set()
 
@@ -265,36 +296,6 @@ class CurlResponse:
         pass
 
 
-def read_headers(header_line: bytes, output: CaseInsensitiveDict):
-    # HTTP standard specifies that headers are encoded in iso-8859-1.
-    # On Python 2, decoding step can be skipped.
-    # On Python 3, decoding step is required.
-    header_line = header_line.decode('iso-8859-1')
-
-    # Header lines include the first status line (HTTP/1.x ...).
-    # We are going to ignore all lines that don't have a colon in them.
-    # This will botch headers that are split on multiple lines...
-    if ':' not in header_line:
-        return
-
-    # Break the header line into header name and value.
-    name, value = header_line.split(':', 1)
-
-    # Remove whitespace that may be present.
-    # Header lines include the trailing newline, and there may be whitespace
-    # around the colon.
-    name = name.strip()
-    value = value.strip()
-
-    # Header names are case insensitive.
-    # Lowercase name here.
-    name = name.lower()
-
-    # Now we can actually record the header name and value.
-    # Note: this only works when headers are not duplicated, see below.
-    output[name] = value
-
-
 def headers_to_list(headers: Dict[str, str]) -> List[str]:
     result = []
     for key, value in headers.items():
@@ -334,12 +335,8 @@ class CurlSession:
     ) -> CurlResponse:
         from b2sdk.b2http import NotDecompressingHTTPAdapter
 
-        # Currently head is incompatible with stream.
-        if method == 'head':
-            stream = False
-
         output = StreamedBytes()
-        output_headers = CaseInsensitiveDict()
+        output_headers = HeaderReader()
 
         curl = pycurl.Curl()
         curl.setopt(pycurl.VERBOSE, self.VERBOSE)
@@ -362,7 +359,7 @@ class CurlSession:
             )
             curl.setopt(pycurl.POSTFIELDSIZE, content_length)
         curl.setopt(pycurl.TIMEOUT_MS, timeout * 1000)
-        curl.setopt(pycurl.HEADERFUNCTION, partial(read_headers, output=output_headers))
+        curl.setopt(pycurl.HEADERFUNCTION, output_headers.add_header_line)
         if any(
             isinstance(adapter, NotDecompressingHTTPAdapter)
             for adapter in self.adapters.adapters.values()

77333b5c8bec0db2cafa72937bf3e7416a86da5f
Krzysztof Kalinowski <krzysztof.kalinowski@reef.pl>
2022-11-28 10:30:18 +0100

    Environmental variables to control used http backend

--- b2sdk/utils/session_protocol.py
+++ b2sdk/utils/session_protocol.py
@@ -9,13 +9,27 @@
 ######################################################################
 import functools
 import importlib
-from typing import Type
+import os
+from typing import (
+    NamedTuple,
+    Type,
+)
+
+
+class ProtocolInfo(NamedTuple):
+    module_name: str
+    class_name: str
+    env_variable: str
+
 
 # This list is to be ordered from the best to the worst.
 # It contains pairs "module" "class" that is to be imported.
-FROM_IMPORT_LIST = [
-    ('b2sdk.utils.curl', 'CurlSession'),
-    ('requests', 'Session'),
+# Third element is name of the environmental variable
+# That, if set, ensures that only this particular protocol
+# is taken into account.
+FROM_IMPORT_ENV_LIST = [
+    ProtocolInfo('b2sdk.utils.curl', 'CurlSession', 'B2_USE_LIBCURL'),
+    ProtocolInfo('requests', 'Session', 'B2_USE_REQUESTS'),
 ]
 
 
@@ -38,7 +52,7 @@ class SessionProtocol:
 
 
 @functools.cache
-def get_session_protocols() -> tuple[list[Type[SessionProtocol]], dict[str, str]]:
+def get_session_protocols(enable_env_checking: bool = True) -> tuple[list[Type[SessionProtocol]], dict[str, str]]:
     """
     Tries to import all available session protocols. Returns a list ordered from "the best"
     to "the worst" and a dictionary with names of protocols that failed with a reason.
@@ -46,12 +60,33 @@ def get_session_protocols() -> tuple[list[Type[SessionProtocol]], dict[str, str]
     protocols = []
     errors = {}
 
-    for module_name, class_name in FROM_IMPORT_LIST:
+    # List of protocols that we should consider.
+    enabled_protocols = set()
+
+    if enable_env_checking:
+        for protocol_info in FROM_IMPORT_ENV_LIST:
+            env_value = os.getenv(protocol_info.env_variable)
+            # We assume that any value assigned to these variables is enough. Only empty string and None are rejected.
+            is_env_set = env_value is not None and len(env_value) > 0
+            if not is_env_set:
+                continue
+            enabled_protocols.add(protocol_info.env_variable)
+
+    for protocol_info in FROM_IMPORT_ENV_LIST:
+        module_class = f'{protocol_info.module_name}.{protocol_info.class_name}'
+
+        if len(enabled_protocols) > 0 and protocol_info not in enabled_protocols:
+            errors[module_class] = f'Module is not on enabled protocols list set via environmental variables.'
+            continue
+
         try:
-            module = importlib.import_module(module_name)
-            protocol = getattr(module, class_name)
+            module = importlib.import_module(protocol_info.module_name)
+            protocol = getattr(module, protocol_info.class_name)
             protocols.append(protocol)
         except (ImportError, AttributeError) as error:
-            errors[f'{module_name}.{class_name}'] = str(error)
+            errors[module_class] = str(error)
+            # This protocol had environmental variable set to enable it, and we failed to load the library.
+            assert protocol_info.env_variable not in enabled_protocols, \
+                f'Unable to enable {protocol_info.env_variable}: {str(error)}'
 
     return protocols, errors

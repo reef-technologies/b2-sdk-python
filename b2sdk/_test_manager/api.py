f7d05cbabb26dc0d948a99401b508ef1d9ffd20d
kamil-gwizdz <kamil.gwizdz@swmansion.com>
2023-04-28 05:40:53 +0200

    Add All Rights Reserved to new files

--- b2sdk/_test_manager/api.py
+++ b2sdk/_test_manager/api.py
@@ -1,3 +1,12 @@
+######################################################################
+#
+# File: b2sdk/_test_manager/api.py
+#
+# Copyright 2023 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
 import contextlib
 import random
 import string
--- b2sdk/_test_manager/bucket_tracking.py
+++ b2sdk/_test_manager/bucket_tracking.py
@@ -1,3 +1,12 @@
+######################################################################
+#
+# File: b2sdk/_test_manager/bucket_tracking.py
+#
+# Copyright 2023 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
 from b2sdk.v2 import Bucket
 
 

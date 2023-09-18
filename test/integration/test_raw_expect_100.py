58662627955dba7939caad424d8414d929e60c67
Enam Mijbah Noor <enam.noor@reef.pl>
2023-09-19 01:08:06 +0600

    add test to verify data is sent AFTER waiting for 100 Continue response

--- test/integration/test_raw_expect_100.py
+++ test/integration/test_raw_expect_100.py
@@ -56,9 +56,7 @@ def test_expect_100_timeout(raw_api, upload_url_dict, http_sent_data):
     data = io.BytesIO(file_contents)
     timeout = 0
 
-    with mock.patch(
-        "urllib3.util.wait_for_read", side_effect=wait_for_read
-    ) as wait_mock:
+    with mock.patch("urllib3.util.wait_for_read", side_effect=wait_for_read) as wait_mock:
         raw_api.upload_file(
             upload_url_dict['uploadUrl'],
             upload_url_dict['authorizationToken'],
@@ -87,9 +85,7 @@ def test_expect_100_disabled(raw_api, upload_url_dict, http_sent_data):
     file_sha1 = hex_sha1_of_stream(io.BytesIO(file_contents), file_length)
     data = io.BytesIO(file_contents)
 
-    with mock.patch(
-        "urllib3.util.wait_for_read", side_effect=wait_for_read
-    ) as wait_mock:
+    with mock.patch("urllib3.util.wait_for_read", side_effect=wait_for_read) as wait_mock:
         raw_api.upload_file(
             upload_url_dict['uploadUrl'],
             upload_url_dict['authorizationToken'],
@@ -107,3 +103,35 @@ def test_expect_100_disabled(raw_api, upload_url_dict, http_sent_data):
         )
     assert file_contents in http_sent_data
     assert wait_mock.call_count == 0
+
+
+def test_expect_100_data_sent_after_wait(raw_api, upload_url_dict, http_sent_data):
+    file_name = 'test-100-continue.txt'
+    file_contents = secrets.token_bytes()
+    file_length = len(file_contents)
+    file_sha1 = hex_sha1_of_stream(io.BytesIO(file_contents), file_length)
+    data = io.BytesIO(file_contents)
+
+    def patched_wait(*args, **kwargs):
+        # verify that, data is not sent before waiting
+        assert file_contents not in http_sent_data, "data sent before waiting for 100 Continue"
+        wait_for_read(*args, **kwargs)
+
+    with mock.patch("urllib3.util.wait_for_read", side_effect=patched_wait) as wait_mock:
+        raw_api.upload_file(
+            upload_url_dict['uploadUrl'],
+            upload_url_dict['authorizationToken'],
+            file_name,
+            file_contents,
+            'text/plain',
+            file_sha1,
+            {'color': 'blue'},
+            data,
+            server_side_encryption=EncryptionSetting(
+                mode=EncryptionMode.SSE_B2,
+                algorithm=EncryptionAlgorithm.AES256,
+            ),
+        )
+    assert file_contents in http_sent_data
+    assert wait_mock.call_count == 1
+    args, _ = wait_mock.call_args
